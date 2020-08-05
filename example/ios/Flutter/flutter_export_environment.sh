ebee41fbc5ba81d6493bdc37ee2e6b1c730c5798
Majid Chenarani <chenarani.majid@gmail.com>
2020-08-05 09:43:55 +0430

    تغیییر ورژن گردل

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/nikk.li/Work/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/wenyan/zeno/project/flutter_audio_recorder/example"
-export "FLUTTER_TARGET=/Users/wenyan/zeno/project/flutter_audio_recorder/example/lib/main.dart"
+export "FLUTTER_ROOT=C:\bootiyar\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\bootiyar\flutter_audio_recorder\example"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/nikk.li/Work/development/flutter/bin/cache/artifacts/engine/ios"
-export "TRACK_WIDGET_CREATION=true"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=C:\bootiyar\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
